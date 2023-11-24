90fd7569ac0a3acd88addcf47c1b517865342ae5
1RahulP <007rahulpundir@gmail.com>
2023-11-25 00:08:00 +0530

    cab9 setup

---
+++ cab9/components/Button.tsx
@@ -0,0 +1,10 @@
+function Button() {
+  return (
+    <>
+      <button className=" text-2xl">
+        <span className="bg-black text-white">submit</span>
+      </button>
+    </>
+  );
+}
+export default Button;
---
+++ cab9/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Button from "../../../components/Button";
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold hover:text-red-500">hii</h1>
+        <Button />
+      </div>
+    </>
+  );
+};
+export default Login;
